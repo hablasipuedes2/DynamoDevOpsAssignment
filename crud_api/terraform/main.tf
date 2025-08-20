e34350c1b6d00a2b5114c50028296f788f6382f9
Valentina Gaydarova <hablasipuedes2@gmail.com>
2025-08-20 12:15:59 +0300

    Add configuration

--- crud_api/terraform/main.tf
+++ crud_api/terraform/main.tf
@@ -22,7 +22,7 @@ module "eks" {
   version = "21.1.0"
 
   name    = var.cluster_name
-  kubernetes_version = "1.30"
+  kubernetes_version = "1.33"
 
   endpoint_public_access = true
 
@@ -36,7 +36,7 @@ module "eks" {
       desired_size = 2
       min_size     = 1
       max_size     = 3
-
+      
       instance_types = var.instance_types
       capacity_type  = "ON_DEMAND"
     }
@@ -48,109 +48,66 @@ module "eks" {
   }
 }
 
-resource "aws_eks_addon" "ebs_csi" {
-  cluster_name             = module.eks.cluster_name
-  addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.47.0-eksbuild.1"
-  service_account_role_arn = module.eks.irsa_role_arn
-}
-
-resource "aws_iam_role_policy" "ebs_csi_irsa_policy" {
-  name = "ebs-csi-irsa-policy"
-  role = module.eks.irsa_role_name  # or irsa_role_arn if you prefer using ARN
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "ec2:CreateVolume",
-          "ec2:AttachVolume",
-          "ec2:DetachVolume",
-          "ec2:DeleteVolume",
-          "ec2:DescribeVolumes",
-          "ec2:DescribeVolumeStatus",
-          "ec2:DescribeInstances",
-          "ec2:ModifyVolume",
-          "ec2:DescribeTags",
-          "ec2:CreateTags",
-          "ec2:DeleteTags"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-# TODO: Migrate to Kubernetes Manifest
-# resource "kubernetes_storage_class" "ebs_sc" {
-#   metadata {
-#     name = "ebs-sc"
-#   }
-
-#   storage_provisioner          = "ebs.csi.aws.com"
-#   reclaim_policy      = "Retain"
-
-#   parameters = {
-#     type = "gp3"
-#   }
-# }
-
-resource "helm_release" "sealed_secrets" {
-  name       = "sealed-secrets"
-  namespace  = "kube-system"
-  repository = "https://bitnami-labs.github.io/sealed-secrets"
-  chart      = "sealed-secrets"
-  version    = "2.15.3" # check latest
-
-  set {
-    name  = "fullnameOverride"
-    value = "sealed-secrets-controller"
-  }
-}
-
-# Assume role policy for Kubernetes ServiceAccount
-data "aws_iam_policy_document" "eks_irsa_sa" {
+data "aws_iam_policy_document" "ebs_csi_assume" {
   statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-    effect  = "Allow"
+    effect = "Allow"
+
     principals {
       type        = "Federated"
-      identifiers = [module.eks.oidc_provider_arn] # from your EKS module
+      identifiers = [module.eks.oidc_provider_arn]
     }
+
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
     condition {
       test     = "StringEquals"
-      variable = "${replace(module.eks.oidc_provider_url, "https://", "")}:sub"
-      values   = ["system:serviceaccount:default:flask-api-sa"]
+      variable = "${module.eks.cluster_oidc_issuer_url}:sub"
+      values   = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
     }
   }
 }
 
-# IAM role for ECR access
-resource "aws_iam_role" "flask_api_role" {
-  name               = "flask-api-role"
-  assume_role_policy = data.aws_iam_policy_document.eks_irsa_sa.json
+resource "aws_iam_role" "ebs_csi_driver" {
+  name = "AmazonEKS_EBS_CSI_DriverRole"
+
+  assume_role_policy = data.aws_iam_policy_document.ebs_csi_assume.json
 }
 
-# IAM policy allowing ECR pull
-resource "aws_iam_role_policy" "flask_api_ecr" {
-  name = "flask-api-ecr-policy"
-  role = aws_iam_role.flask_api_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "ecr:GetAuthorizationToken",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchGetImage"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
+resource "aws_iam_role_policy_attachment" "ebs_csi_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.ebs_csi_driver.name
 }
+
+# resource "aws_eks_addon" "ebs_csi" {
+#   cluster_name             = module.eks.cluster_name
+#   addon_name               = "aws-ebs-csi-driver"
+#   service_account_role_arn = aws_iam_role.ebs_csi_driver.arn
+# }
+
+# # IAM role for ECR access
+# resource "aws_iam_role" "flask_api_role" {
+#   name               = "flask-api-role"
+#   assume_role_policy = data.aws_iam_policy_document.eks_irsa_sa.json
+# }
+
+# # IAM policy allowing ECR pull
+# resource "aws_iam_role_policy" "flask_api_ecr" {
+#   name = "flask-api-ecr-policy"
+#   role = aws_iam_role.flask_api_role.id
+
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Action = [
+#           "ecr:GetAuthorizationToken",
+#           "ecr:BatchCheckLayerAvailability",
+#           "ecr:GetDownloadUrlForLayer",
+#           "ecr:BatchGetImage"
+#         ]
+#         Resource = "*"
+#       }
+#     ]
+#   })
+# }
